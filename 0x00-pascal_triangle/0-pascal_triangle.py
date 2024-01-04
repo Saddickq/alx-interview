1b2160b88be30010ca3d37be38fcec2b38b3a61d
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-01-04 11:03:02 +0000

    pycodestyle refactoring

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,15 +4,17 @@
 def pascal_triangle(n):
     triangle = []
 
-    if (n > 0):
-        for i in range(n):
-            row = []
-            for j in range(i + 1):
-                if (j == 0 or j == i):
-                    row.append(1)
-                else:
-                    nextvalue = triangle[i - 1][j - 1] + triangle[i - 1][j]
-                    row.append(nextvalue)
-            triangle.append(row)
+    if (n <= 0):
+        return triangle
+
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if (j == 0 or j == i):
+                row.append(1)
+            else:
+                nextvalue = triangle[i - 1][j - 1] + triangle[i - 1][j]
+                row.append(nextvalue)
+        triangle.append(row)
 
     return triangle
