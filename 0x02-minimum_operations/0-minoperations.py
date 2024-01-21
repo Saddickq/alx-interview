3ed5c6a5c95fccd07dbd3bd03eb920fb541ebb95
Abubakr Saddiq <abubakrsaddickq@gmail.com>
2024-01-21 22:14:27 +0000

    0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" a method that calculates the fewest
+number of operations needed to result
+in exactly n H characters in the file."""
+
+
+def minOperations(n: int) -> int:
+    """ function documentation"""
+
+    if type(n) != int or n < 0:
+        return 0
+
+    min_ops = 0
+    operations = 1
+    copy = 0
+
+    while operations < n:
+        if n % operations == 0:
+            copy = operations
+            operations += copy
+            min_ops += 2
+        else:
+            operations += copy
+            min_ops += 1
+
+    return min_ops
