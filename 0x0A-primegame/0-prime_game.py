e758d0c7c553ae05ad716c3e149eb89f5b9a54bb
saddickq <abubakrsaddickq@gmail.com>
2024-03-17 06:54:23 -0700

    prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Maria and Ben are playing a game"""
+
+
+def is_winner(rounds, numbers):
+    """Determines the winner of the game.
+
+    Args:
+        rounds (int): Number of rounds.
+        numbers (list of int): List of numbers.
+
+    Returns:
+        str or None: Name of the winner ("Ben" or "Maria") or None if inputs are invalid.
+    """
+    if rounds <= 0 or numbers is None or rounds != len(numbers):
+        return None
+
+    ben_score = 0
+    maria_score = 0
+
+    # Sieve of Eratosthenes to generate primes
+    primes = generate_primes(max(numbers))
+
+    for num in numbers:
+        if sum(primes[:num + 1]) % 2 == 0:
+            ben_score += 1
+        else:
+            maria_score += 1
+
+    if ben_score > maria_score:
+        return "Ben"
+    elif maria_score > ben_score:
+        return "Maria"
+    else:
+        return None
+
+
+def generate_primes(limit):
+    """Generates prime numbers using Sieve of Eratosthenes.
+
+    Args:
+        limit (int): Upper limit for generating primes.
+
+    Returns:
+        list of int: List containing 1 at prime indices and 0 at composite indices.
+    """
+    primes = [1] * (limit + 1)
+    primes[0], primes[1] = 0, 0
+
+    for i in range(2, len(primes)):
+        for j in range(2 * i, len(primes), i):
+            primes[j] = 0
+
+    return primes
+
